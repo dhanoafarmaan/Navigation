8f2b40272cd3f78bede3ee849b4e3affb59786ee
Farmaan Dhanoa <dhanoafarmaan@gmail.com>
2026-08-03 16:02:56 -0400

    added variablity to amount of nodes for testing

--- Nodes.py
+++ Nodes.py
@@ -1,9 +1,18 @@
 # The keys ('A', 'B', etc.) represent the nodes.
 # The inner dictionaries represent outgoing connections and their weights.
-graph = {
-    'A': {'B': 4, 'C': 2},
-    'B': {'C': 1, 'D': 5},
-    'C': {'B': 1, 'D': 8, 'E': 10},
-    'D': {'E': 2},
-    'E': {}
-}
+import random
+
+num_nodes = 100   # Change this to any number
+
+graph = {}
+
+for i in range(num_nodes):
+    node = f"A{i}"
+    graph[node] = {}
+
+    if i + 1 < num_nodes:
+        graph[node][f"A{i+1}"] = random.randint(1, 20)
+    if i + 2 < num_nodes:
+        graph[node][f"A{i+2}"] = random.randint(1, 20)
+    if i + 3 < num_nodes:
+        graph[node][f"A{i+3}"] = random.randint(1, 20)
